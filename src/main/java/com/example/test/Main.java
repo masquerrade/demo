c9d2562c35b00cc8e7bd17dfb53221645088e825
masquerraade <mayankkmr82@gmail.com>
2025-10-28 22:23:18 +0530

    Started course schedule

--- src/main/java/com/example/test/Main.java
+++ src/main/java/com/example/test/Main.java
@@ -11,26 +11,39 @@ import java.util.Set;
 // Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-
-class CustomSet<T> extends HashSet<T>{
-
-        private int c=0;
-        public CustomSet(){
-            super();
-        }
-
-        @Override
-        public boolean add(T t){
-            c++;
-            return super.add(t);
-
-        }
-
-        public int  getCount(){
-            return c;
-        }
-
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+      
+      //First thing I need to do is using the prerequisites array create an adjacency matrix which will be a list of list.Index will represent the vertex and the list will represent all the courses for which the current index is the prerequsite.
+      
+      //Iterate through the prerequisite list and for each prerquusite assign the courses that is add the edge.
+      
+      List<List<Integer>> lst=new ArrayList<>();
+      
+      //First I need to initialise all the List
+      
+      int n=prerequisite.size();
+      int [] indegree=new int[n];
+      
+      for(int i=0;i<n;i++){
+        lst.add(i,new ArrayList<>());
+      }
+      
+      //Here my lst is created
+      for(int [] l:lst){
+        lst.get(l[1]).append(l[0]);
+        indegree[l[0]]++;
+      }
+      
+      //Now I need to add to the queue the courses for which there are no prereq
+      
+      //For each course I need to maintain how many prereq are remaining.I need this for topological sorting
+      
+      
+      
     }
+}
+
 
 public class Main{
 
