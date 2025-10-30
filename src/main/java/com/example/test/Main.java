f873084bf2848758c59fd5966b7467bf99f7cc11
masquerraade <mayankkmr82@gmail.com>
2025-10-30 22:22:03 +0530

    Final DFS

--- src/main/java/com/example/test/Main.java
+++ src/main/java/com/example/test/Main.java
@@ -15,6 +15,9 @@ import java.util.Set;
 // Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
+
+//BFS approach
+//
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
       
