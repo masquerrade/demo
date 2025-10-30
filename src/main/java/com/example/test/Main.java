c9296655ef400a8040b4ee16eaf3389214905e60
Mayank Kumar <mayankkmr82@gmail.com>
2025-10-30 21:54:04 +0530

    Finally complete

--- src/main/java/com/example/test/Main.java
+++ src/main/java/com/example/test/Main.java
@@ -1,7 +1,11 @@
 package com.example.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -22,20 +26,52 @@ class Solution {
       
       //First I need to initialise all the List
       
-      int n=prerequisite.size();
-      int [] indegree=new int[n];
+      int n=prerequisites.length;
+      int [] indegree=new int[numCourses];
+
       
-      for(int i=0;i<n;i++){
+      for(int i=0;i<numCourses;i++){
         lst.add(i,new ArrayList<>());
       }
       
       //Here my lst is created
-      for(int [] l:lst){
-        lst.get(l[1]).append(l[0]);
+      for(int [] l:prerequisites){
+        lst.get(l[1]).add(l[0]);
         indegree[l[0]]++;
       }
+
+      
       
       //Now I need to add to the queue the courses for which there are no prereq
+      //First pass I need to add courses with no prerequisite.
+      
+      //First define a queue
+      
+      Queue<Integer> bfs=new LinkedList<>();
+      
+      for(int i=0;i<numCourses;i++){
+        if(indegree[i]==0){
+          bfs.offer(i);
+        }
+      }
+      int takenCourses=0;
+      while(!bfs.isEmpty()){
+        int e=bfs.poll();
+        takenCourses++;
+        
+        //Go through all the elm in the adj list and reduce the incourse
+        
+        for(int i:lst.get(e)){
+          indegree[i]--;
+          if(indegree[i]==0){
+            bfs.offer(i);
+          }
+          
+        }
+        
+      }
+      
+      return takenCourses==numCourses;
       
       //For each course I need to maintain how many prereq are remaining.I need this for topological sorting
       
@@ -49,27 +85,13 @@ public class Main{
 
 
     public static void main(String [] args) {
-            Main m1 =new Main();
-            CustomSet<Integer> s=new CustomSet<>();
-            insertObject(s);
-            System.out.println(s.getCount());
-        
+            Solution s=new Solution();
+            System.out.println(s.canFinish(2,new int[][]{{1,0},{0,1}}));
+                  
    
     }
 
-    
-
-     public static void insertObject(Set<Integer> set){
-        set.add(100);
-        set.add(300);
-        set.add(200);
-        set.add(100);
-        set.add(100);
-        set.add(300);
-        set.add(400);
-     }
- 
-
+   
 
 }
 
